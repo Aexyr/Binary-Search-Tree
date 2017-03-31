d0b9f37d9350b4fba4d7a6e95cd6f42344fb9366
Aexyr <nlauguico@gmail.com>
2017-03-31 03:46:29 -0700

    Added VS Project

---
+++ Project/BST.cpp
@@ -0,0 +1,186 @@
+#include "BST.h"
+
+BST::BST() : root(nullptr) {}
+
+//void BST::insert(int insertItem)
+//{
+//	Node  *newNode = new Node;
+//	newNode->data = insertItem;
+//	newNode->llink = nullptr;
+//	newNode->rlink = nullptr;
+//	insert(root, newNode);
+//}
+
+//void BST::insert( Node* &p, Node *newNode)
+//{
+//	if (p == nullptr)
+//		p = newNode;
+//	else if (p->data == newNode->data)
+//		cerr << "No duplicates are allowed." << endl;
+//	else if (p->data > newNode->data)
+//		insert(p->llink, newNode);
+//	else
+//		insert(p->rlink, newNode);
+//}
+
+void BST::inorderTraversal() const
+{
+	if (root == nullptr)
+		cerr << "There is no tree.";
+	else
+	{
+		inorderTraversal(root);
+	}
+}
+
+void BST::inorderTraversal(const Node *p) const
+{
+	if (p != nullptr)
+	{
+		inorderTraversal(p->llink);
+		cout << p->data << " ";
+		inorderTraversal(p->rlink);
+	}
+}
+
+void  BST::destroyTree(Node* &p)
+{
+	if (p != nullptr)
+	{
+		destroyTree(p->llink);
+		destroyTree(p->rlink);
+		delete p;
+		p = nullptr;
+	}
+}
+	
+void  BST::destroyTree()
+{
+	destroyTree(root);
+}
+
+BST::~BST()
+{
+	destroyTree(root);
+}
+
+
+/***************************************************************
+
+	Implement your functions below this line.
+
+***************************************************************/
+
+// Definition function insert (non-recursive) 
+void BST::insert(int item)
+{
+	if (root != nullptr)
+	{
+		Node* temp = root;
+		bool isDone = false;
+		do {
+			if (item == temp->data)
+			{
+				cout << "The item to insert is already in the list – duplicates are not allowed." << endl;
+				isDone = true;
+			}
+			else if (item < temp->data)
+			{
+				if (temp->llink == nullptr)
+				{
+					temp->llink = new Node;
+					temp->llink->data = item;
+					isDone = true;
+				}
+				else
+				{
+					temp = temp->llink;
+				}
+			}
+			else
+			{
+				if (temp->rlink == nullptr)
+				{
+					temp->rlink = new Node;
+					temp->rlink->data = item;
+					isDone = true;
+				}
+				else
+				{
+					temp = temp->rlink;
+				}
+			}
+		} while (!isDone);
+	}
+	else
+	{
+		root = new Node;
+		root->data = item;
+	}
+}
+
+// Definition function totalNodes
+int BST::totalNodes() const
+{
+	if (root == nullptr)
+	{
+		return 0;
+	}
+	
+	return totalNodes(root) - 1;
+}
+
+// Definition function totalNodes (recursive)
+int BST::totalNodes(Node * p) const
+{
+	if (p == nullptr)
+	{
+		return 1;
+	}
+
+	return totalNodes(p->llink) + totalNodes(p->rlink);
+}
+
+// Definition overloaded function preorderTraversal
+void BST::preorderTraversal() const
+{
+	if (root == nullptr)
+		cerr << "There is no tree.";
+	else
+	{
+		preorderTraversal(root);
+	}
+}
+
+// Definition overloaded function preorderTraversal (recursive)
+void BST::preorderTraversal(const Node * p) const
+{
+	if (p != nullptr)
+	{
+		cout << p->data << " ";
+		preorderTraversal(p->llink);
+		preorderTraversal(p->rlink);
+	}
+}
+
+// Definition overloaded function postorderTraversal
+void BST::postorderTraversal() const
+{
+	if (root == nullptr)
+		cerr << "There is no tree.";
+	else
+	{
+		postorderTraversal(root);
+	}
+}
+
+// Definition overloaded function postorderTraversal (recursive)
+void BST::postorderTraversal(const Node * p) const
+{
+	if (p != nullptr)
+	{
+		preorderTraversal(p->llink);
+		preorderTraversal(p->rlink);
+		cout << p->data << " ";
+	}
+}
